20ef03509df30f814ecc298662527ced1d008a31
David C Black <david.black@doulos.com>
2021-05-16 15:54:06 +0000

    Suppress warnings unless -v specified

--- .local/bin/project_init.sh
+++ .local/bin/project_init.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 
-TIMESTAMP="$(date +%y%m%d-%H%M%S)"
-hostname
-for DOTFILE in bash_aliases vim vimrc; do
-  if [[ -h "${HOME}/.${DOTFILE}" ]]; then
-    echo "WARNING: Leaving .${DOTFILE} in place"
-    continue;
-  elif [[ -d "${HOME}/.${DOTFILE}" ]]; then
-    echo "WARNING: Leaving .${DOTFILE} in place"
-    continue;
-  elif [[ -f "${HOME}/.${DOTFILE}" ]]; then
-    bash -v -c "mv '${HOME}/.${DOTFILE}' '${HOME}/.${DOTFILE}-${TIMESTAMP}'";
+function project_init() {
+  local TIMESTAMP="$(date +%y%m%d-%H%M%S)"
+  local VERBOSE=0
+  if [[ "$1" == "-v" ]]; then
+    VERBOSE=1
   fi
-  bash -v -c "ln -s '${HOME}/.local/dotfiles/${DOTFILE}' '${HOME}/.${DOTFILE}'";
-done
+  if [[ $VERBOSE == 1 ]]; echo "Host: $(hostname)"; fi
+  for DOTFILE in bash_aliases vim vimrc; do
+    if [[ -h "${HOME}/.${DOTFILE}" ]]; then
+      if [[ $VERBOSE == 1 ]]; then echo "WARNING: Leaving .${DOTFILE} in place"; fi
+      continue;
+    elif [[ -d "${HOME}/.${DOTFILE}" ]]; then
+      if [[ $VERBOSE == 1 ]]; then echo "WARNING: Leaving .${DOTFILE} in place"; fi
+      continue;
+    elif [[ -f "${HOME}/.${DOTFILE}" ]]; then
+      bash -v -c "mv '${HOME}/.${DOTFILE}' '${HOME}/.${DOTFILE}-${TIMESTAMP}'";
+    fi
+    bash -v -c "ln -s '${HOME}/.local/dotfiles/${DOTFILE}' '${HOME}/.${DOTFILE}'";
+  done
+}
+
+project_init
