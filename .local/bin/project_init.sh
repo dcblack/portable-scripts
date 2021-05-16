6b36a5d110aa607ab26d50fcc3b4243b5c59b746
David C Black <david.black@doulos.com>
2021-05-16 11:00:09 -0500

    Fix syntax

--- .local/bin/project_init.sh
+++ .local/bin/project_init.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 function project_init() {
-  local TIMESTAMP="$(date +%y%m%d-%H%M%S)"
-  local VERBOSE=0
-  if [[ "$1" == "-v" ]]; then VERBOSE=1 fi
-  if [[ $VERBOSE == 1 ]]; then echo "Host: $(hostname)"; fi
+  local TIMESTAMP VERBOSE
+  TIMESTAMP="$(date +%y%m%d-%H%M%S)";
+  VERBOSE=0;
+  if [[ "$1" == "-v" ]]; then VERBOSE=1; fi
+  if [[ ${VERBOSE} == 1 ]]; then echo "Host: $(hostname)"; fi
   for DOTFILE in bash_aliases vim vimrc; do
     if [[ -h "${HOME}/.${DOTFILE}" ]]; then
-      if [[ $VERBOSE == 1 ]]; then echo "WARNING: Leaving .${DOTFILE} in place"; fi
+      if [[ ${VERBOSE} == 1 ]]; then echo "WARNING: Leaving .${DOTFILE} in place"; fi
       continue;
     elif [[ -d "${HOME}/.${DOTFILE}" ]]; then
-      if [[ $VERBOSE == 1 ]]; then echo "WARNING: Leaving .${DOTFILE} in place"; fi
+      if [[ ${VERBOSE} == 1 ]]; then echo "WARNING: Leaving .${DOTFILE} in place"; fi
       continue;
     elif [[ -f "${HOME}/.${DOTFILE}" ]]; then
       bash -v -c "mv '${HOME}/.${DOTFILE}' '${HOME}/.${DOTFILE}-${TIMESTAMP}'";
@@ -19,4 +20,4 @@ function project_init() {
   done
 }
 
-project_init
+project_init "$@"
