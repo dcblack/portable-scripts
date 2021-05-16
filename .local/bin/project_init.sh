3c5688b6bacc2d33340c231d7f8978782033123e
David C Black <david.black@doulos.com>
2021-05-16 10:32:20 -0500

    Debugged

--- .local/bin/project_init.sh
+++ .local/bin/project_init.sh
@@ -10,9 +10,7 @@ for DOTFILE in bash_aliases vim vimrc; do
     echo "WARNING: Leaving .${DOTFILE} in place"
     continue;
   elif [[ -f "${HOME}/.${DOTFILE}" ]]; then
-    echo "INFO: Backup ${HOME}/.${DOTFILE}-${TIMESTAMP}";
-    mv "${HOME}/.${DOTFILE}" "${HOME}/.${DOTFILE}-${TIMESTAMP}";
+    bash -v -c "mv '${HOME}/.${DOTFILE}' '${HOME}/.${DOTFILE}-${TIMESTAMP}'";
   fi
-  echo "INFO: Linking ${HOME}/.${DOTFILE}-${TIMESTAMP}";
-  ln -s "${HOME}/.local/dotfiles/${DOTFILE}" "${HOME}/.${DOTFILE}"
+  bash -v -c "ln -s '${HOME}/.local/dotfiles/${DOTFILE}' '${HOME}/.${DOTFILE}'";
 done
