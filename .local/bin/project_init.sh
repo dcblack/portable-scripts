51f2ad545fe317fef5f59f22f3f05b208ada8b35
David C Black <david.black@doulos.com>
2021-05-19 06:31:00 -0500

    Add .agignore to bypass .snapshots results

--- .local/bin/project_init.sh
+++ .local/bin/project_init.sh
@@ -6,6 +6,8 @@ function project_init() {
   VERBOSE=0;
   if [[ "$1" == "-v" ]]; then VERBOSE=1; fi
   if [[ ${VERBOSE} == 1 ]]; then echo "Host: $(hostname)"; fi
+  
+  # Add symbolic links to select "dot" files
   for DOTFILE in bash_aliases vim vimrc; do
     if [[ -h "${HOME}/.${DOTFILE}" ]]; then
       if [[ ${VERBOSE} == 1 ]]; then echo "WARNING: Leaving .${DOTFILE} in place"; fi
@@ -17,6 +19,17 @@ function project_init() {
       bash -v -c "mv '${HOME}/.${DOTFILE}' '${HOME}/.${DOTFILE}-${TIMESTAMP}'";
     fi
     bash -v -c "ln -s '${HOME}/.local/dotfiles/${DOTFILE}' '${HOME}/.${DOTFILE}'";
+  done
+  
+  # Make sure `ag` (aka silver-searcher) doesn't search .snapshots
+  AGIGNORE="${HOME}/.agignore"
+  for IGNORED in .snapshots; do
+    if [[ -r "${AGIGNORE}" ]]; then
+      grep -sqF "${IGNORED}" "${AGIGNORE}" \
+      || echo "${IGNORED}" >> "${AGIGNORE}"
+    else
+      echo "${IGNORED}" > "${AGIGNORE}"
+    fi
   done
 }
 
