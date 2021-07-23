072a4a8fee8fc362950fac87e24713a8b1689ed9
David C Black <david.black@doulos.com>
2021-07-23 16:20:37 -0500

    Bunch of changes. Evolving rather quickly.

--- .local/ABOUT.md
+++ .local/ABOUT.md
@@ -3,7 +3,11 @@ The portable-scripts collection
 
 Scripts used to setup a familiar environment on new Linux hosts and build favorite tools. These should be useful to Doulos instructors. These should work on any Ubuntu installation including AWS, CoCalc and Docker. They are designed to work when you do not have `sudo` or `root` privileges.
 
-It may not be obvious, but most of the utility is under a directory called `.local`. The intent is that you might locate it under `$HOME` or perhaps more profitably, create a symbolic link to it from `$HOME`.
+It may not be obvious, but most of the utility is under a directory called `.local`. The intent is that you locate a symbolic link to `$(pwd)/portablescripts/.local` under `$HOME`. You could also copy or move it there, but that might make it more difficult to update. If you really want to move it to `$HOME`, consider putting the entire contents of `portablescripts` there. A simple approach to this latter approach would be:
+
+```bash
+git clone https/github.com/dcblack/portable-scripts.git "${HOME}"
+```
 
 WARNING: If symbolic links are employed, take care to avoid renaming or moving the location of this directory, lest you destroy critical functionality (e.g., .bashrc).
 
@@ -21,18 +25,19 @@ The following describes some of the contents. most of the directories have a `RE
 dcblack_doulos.gpg ── my public GPG key
 setup.profile
 .local/
-├── ARCHIVES/Ø ─ holds stuff downloaded by scripts
-├── apps/ ─── default location for installation
-│   ├── cmake/ ─ use with cmake esp. for SystemC
-│   └── sc-templates/ ─ see `bin/new` script for more information
-├── bin/ ─ utilities (esp. scripts to build tools from source)
-├── dotfiles/ ─ replacements for .bashrc, .vimrc, etc.
-├── lib/ ─ library stuff (e.g., for Perl)
-├── misc/ ─ unclassified
-├── scratch/Ø ─ place to put junk/temporaries
-├── share/Ø ─ used by some installers
-├── src/Ø ─ place to hold source used during builds
-└── tests/ ───────────── Contains scripts that test things in bin/
+├── ARCHIVES/ ────────── holds stuff downloaded by scripts
+├── apps/ ────────────── default location for installation
+│   ├── cmake/ ───────── use with cmake esp. for SystemC
+│   ├── sc_templates/ ── use with cmake esp. for SystemC
+│   └── src/ ─────────── see `bin/new` script for more information
+├── bin/ ─────────────── utilities (esp. scripts to build tools from source)
+├── dotfiles/ ────────── replacements for .bashrc, .vimrc, etc.
+├── lib/ ─────────────── library stuff (e.g., for Perl)
+├── misc/ ────────────── unclassified
+├── scratch/ ─────────── place to put junk/temporaries
+├── share/ ───────────── used by some installers
+├── src/ ─────────────── place to hold source used during builds
+└── tests/ ───────────── contains scripts that test things in bin/
 ```
 
 ##### The end
--- .local/bin/utils.bash
+++ .local/bin/utils.bash
@@ -6,6 +6,8 @@ SYNOPSIS
 
 `utils.bash` - A collection of bash functions useful for scripting.
 
+Note: Capitalizing function names reduces collisions with scripts/executables.
+
 | FUNCTION SYNTAX            | DESCRIPTION 
 | :------------------------- | :---------- 
 | Require _SCRIPT_           | Sources a script or gives an error if missing
@@ -13,11 +15,12 @@ SYNOPSIS
 | Logfile [--append] _FILE_  | Establishes a logfile name
 | Log [-n] "_MESSAGE_"       | Adds message to logfile
 | Echo [-n] "_MESSAGE_"      | Displays and logs message
-| Do _COMMAND_               | Displays and executes
+| _do _COMMAND_              | Displays and executes
 | Pass                       | Sets success status (0)
 | Fail                       | Sets error status (1)
 | PassFail "_MESSAGE_"       | Displays message with pass/fail status
 | Info "_MESSAGE_"           | Echo an informational message
+| Comment "_MESSAGE_"        | Does nothing but provide NOP comment
 | Debug "_MESSAGE_"          | Echo a debug message
 | Ruler [-CLR] [_MESSAGE_]   | Echo a ruler with option embedded message
 | Die "_MESSAGE_"            | Echo a fatal message and exit with fail
@@ -26,8 +29,11 @@ SYNOPSIS
 | Summary PROG ["_MESSAGE_"] | Echo a summary of errors and warnings
 | GetBuildOpts "$0" "$@"     | Parses standard _build_ command-line inputs
 | ShowBuildOpts              | Display options variables from GetBuildOpts
-| ConfirmBuildOpts           | Asks user to confirm build locations
+| ConfirmBuildOpts || exit   | Asks user to confirm build locations
 | SetupLogdir _BASENAME_     | Sets up the logfile directory
+| GetSource_and_Cd DIR URL   | Downloads souce and enters directory
+| Generate                   | Invokes cmake or autotools
+| Cleanup_Source             | Removes source
 
 USAGE
 =====
@@ -58,6 +64,7 @@ export CC
 export CLEAN
 export CLEANUP
 export CMAKE_CXX_STANDARD
+export CMAKE_INSTALL_PREFIX
 export CXX
 export DEBUG
 export ERRORS
@@ -65,14 +72,92 @@ export LOGDIR
 export LOGFILE
 export NOTREALLY
 export SRC
+export SYSTEMC_HOME
 export SUFFIX
+export TOOL_NAME
+export TOOL_INFO
+export TOOL_VERS
+export TOOL_URL
+export BUILD_DIR
+export GENERATOR
+export NOINSTALL
 export UNINSTALL
 export VERBOSITY
 export WARNINGS
 export NONE BOLD UNDR CBLK CRED CGRN CYLW CBLU CMAG CCYN CWHT CRED
 
+# Following is 'just in case you did not define this'
 function Realpath () {
+#@ Output the realpath name treating all arguments as a single filename specification
   /usr/bin/perl '-MCwd(abs_path)' -le "print abs_path(qq($*))"
+}
+
+function Firstreal() {
+#@ Output the first argument that names an existing file.
+  perl -le '@_=split($;,join($;,@ARGV));for(@_){next unless -e $_;print $_;exit 0;}' "$@"
+}
+
+function Has_path() {
+  # USAGE: Has_path VAR PATH
+  local arg plscript
+  arg="$(Realpath "$2")"
+  # shellcheck disable=SC2016
+  plscript='$v=$ARGV[0]; $p=$ARGV[1]; for $d (split(qq{:},$ENV{$v})) { next if !-d $d; exit 0 if$p eq abs_path($d); } exit 1'
+  if [[ "${arg}" == "" ]]; then return 1; fi
+  perl -M'Cwd(abs_path)' -le "${plscript}" "$1" "${arg}"
+}
+
+function Prepend_path() { # only if 2nd arg does not exist in first
+  # USAGE: Prepend_path VAR PATH
+  local arg plscript
+  arg="$(Realpath "$2")"
+  # shellcheck disable=SC2016
+  plscript='print qq{$ARGV[0]="$ARGV[1]:$ENV{$ARGV[0]}"; export $ARGV[0]}'
+  Has_path "$1" "$2" || \
+    eval "$(perl -le "${plscript}" "$1" "${arg}")"
+}
+
+function Append_path() { # only if 2nd arg does not exist in first
+  # USAGE: Append_path VAR PATH
+  local var arg plscript
+  var="$1"
+  arg="$(Realpath "$2")"; shift
+  # shellcheck disable=SC2016
+  plscript='print qq{$ARGV[0]="$ENV{$ARGV[0]}:$ARGV[1]"; export $ARGV[0]}'
+  Has_path "$1" "$2" || \
+    eval "$(perl -le "${plscript}" "${var}" "${arg}")"
+}
+
+function Unique_path() {
+  # USAGE: unique_path VAR
+  local plscript
+  # shellcheck disable=SC2016
+  plscript='$v=$ARGV[0]; for my $d(split(qr{:},$ENV{$v})){ next if !-d $d; $e=abs_path($d); if( ! exists $e{$e} ){ $e{$e}=1; push(@e,$e); } } printf qq{%s="%s"\n},$v,join(":",@e);'
+  eval "$(perl -M'Cwd(abs_path)' -e "${plscript}" "$1")"
+}
+
+function Remove_path() {
+  # USAGE: remove_path VAR PATH
+  local plscript
+  # shellcheck disable=SC2016
+  plscript=' $v=$ARGV[0]; $p=abs_path($ARGV[1]); for (split(qr":",$ENV{$v})) { $e=abs_path($_); if($p ne $e) { $push(@e,$e); } } print "$v=",join(":",@e) '
+  eval "$(perl -M'Cwd(abs_path)' -e "${plscript}" "$1" "$2")"
+}
+
+function Add_prefix() {
+  local prefix="$1"
+  shift
+  for element in "$@"; do
+    echo "${prefix}${element}"
+  done
+}
+
+function Add_suffix() {
+  local suffix="$1"
+  shift
+  for element in "$@"; do
+    echo "${element}${suffix}"
+  done
 }
 
 function Require() { # FILE(S) to source
@@ -205,7 +290,7 @@ function Printf() {
   if [[ -f "${LOGFILE}" ]]; then printf "$@" >>"${LOGFILE}"; fi
 }
 
-function Do() {
+function _do() {
   local NX
   if [[ -n "${NOTREALLY}" ]]; then NX="-"; fi
   if [[ "$1" == "-n" ]]; then NX="-"; shift; fi
@@ -244,6 +329,8 @@ function Info() {
     -grn) PRE="${CGRN}" ; shift ;;
     -ylw) PRE="${CYLW}" ; shift ;;
     -blu) PRE="${CBLU}" ; shift ;;
+    -mag) PRE="${CMAG}" ; shift ;;
+    -wht) PRE="${CWHT}" ; shift ;;
     *) PRE="${NONE}" ;;
   esac
   Echo "${CGRN}Info: ${PRE}$*${NONE}"
@@ -326,7 +413,7 @@ function Warn() {
 
 function Summary() {
   Assert $# != 0
-  Echo "Execution summary for $(basename "$1"):"
+  Echo "Execution summary for ${1}:"
   shift
   Echo " ${WARNINGS} warnings"
   Echo -n "  ${ERRORS} errors"
@@ -372,6 +459,7 @@ function ShowBuildOpts() {
   ShowVar CLEAN
   ShowVar CLEANUP
   ShowVar CMAKE_CXX_STANDARD
+  ShowVar CMAKE_INSTALL_PREFIX
   ShowVar CC
   ShowVar CXX
   ShowVar DEBUG
@@ -379,7 +467,15 @@ function ShowBuildOpts() {
   ShowVar LOGFILE
   ShowVar NOTREALLY
   ShowVar SRC
+  ShowVar SYSTEMC_HOME
   ShowVar SUFFIX
+  ShowVar TOOL_NAME
+  ShowVar TOOL_INFO
+  ShowVar TOOL_VERS
+  ShowVar TOOL_URL
+  ShowVar BUILD_DIR
+  ShowVar GENERATOR
+  ShowVar NOINSTALL
   ShowVar UNINSTALL
   ShowVar VERBOSITY
   Ruler -blu
@@ -425,6 +521,7 @@ function GetBuildOpts() {
 #|
 #|  Option             |  Alternative      | Description
 #|  ------             |  ------------     | -----------
+#|  --build-dir=DIR    |  -bd DIR          | source subdirectory to build in
 #|  --cc=C_COMPILER    |  CC=C_COMPILER    | chooses C compiler executable
 #|  --clang            |                   | quick --cc=clang --cxx=clang++
 #|  --clean            |  -clean           | reinstall source
@@ -433,14 +530,20 @@ function GetBuildOpts() {
 #|  --debug            |  -d               | developer use
 #|  --default          |                   | quick -i=$HOME/.local -src=$HOME/.local/src
 #|  --gcc              |                   | quick --cc=gcc --cxx=g++
+#|  --generator=TYPE   |  -gen TYPE        | cmake or autotools
 #|  --home             |                   | quick -i $HOME -s $HOME/src
+#|  --info=TEXT        |  -info TEXT       | choose installation directory
 #|  --install=DIR      |  -i DIR           | choose installation directory
 #|  --notreally        |  -n               | don't execute, just show possibilities
 #|  --src=DIR          |  -s DIR           | choose source directory
 #|  --std=N            |  -std=N           | set make C++ compiler version where N={98,11,14,17,20,...}
+#|  --systemc=DIR      |  -sc              | reference SystemC installation
 #|  --suffix=TEXT      |  -suf TEXT        | set suffix for installation name
+#|  --tool=NAME        |  -tool NAME       | set the desired tool name for tool source
 #|  --uninstall        |  -rm              | remove if possible -- not always supported
+#|  --url=URL          |  -url URL         | set the URL for the source code
 #|  --verbose          |  -v               | echo more information (may be repeated)
+#|  --version=X.Y.Z    |  -vers X.Y.Z      | set the desired tool version
 #|  --quiet            |  -q               | echo less information (may be repeated)
 #|
 #|OUTPUTS
@@ -452,15 +555,24 @@ function GetBuildOpts() {
 #| - $CC
 #| - $CLEAN
 #| - $CLEANUP
-#| - $CMAKE_CXX_STANDARD {98,03,11,14,17,20}
+#| - $CMAKE_CXX_STANDARD {98|03|11|14|17|20}
+#| - $CMAKE_INSTALL_PREFIX
 #| - $CXX
 #| - $DEBUG
 #| - $ERRORS integer
 #| - $LOGDIR
 #| - $LOGFILE
 #| - $NOTREALLY
+#| - $NOINSTALL
 #| - $SRC directory
+#| - $SYSTEMC_HOME
 #| - $SUFFIX
+#| - $TOOL_NAME
+#| - $TOOL_INFO
+#| - $TOOL_VERS
+#| - $BUILD_DIR
+#| - $GENERATOR {cmake|autotools}
+#| - $TOOL_URL
 #| - $UNINSTALL
 #| - $VERBOSITY integer
 #| - $WARNINGS integer
@@ -506,9 +618,24 @@ function GetBuildOpts() {
       HelpText "${0}";
       exit 0
       ;;
-    -n|--notreally)
+    -n|--not-really|--notreally)
       NOTREALLY="-n"
       shift
+      ;;
+    -no-install|--no-install|--noinstall)
+      NOINSTALL="-n"
+      shift
+      ;;
+    --build-dir=*|-bd)
+      if [[ "$1" != '-bd' ]]; then
+        BUILD_DIR="${1//*=}"
+      elif [[ $# -gt 1 && -d "$2" ]]; then
+        BUILD_DIR="$2"
+        shift
+      else
+        Die "Need argument for $1"
+      fi
+      shift;
       ;;
     --cc=*|CC=*)
       CC="${1//*=}"
@@ -555,6 +682,28 @@ function GetBuildOpts() {
       CXX=g++
       shift
       ;;
+    --generator=*|-gen)
+      if [[ "$1" != '-gen' ]]; then
+        GENERATOR="${1//*=}"
+      elif [[ $# -gt 1 && -d "$2" ]]; then
+        GENERATOR="$2"
+        shift
+      else
+        Die "Need argument for $1"
+      fi
+      shift;
+      ;;
+    --info=*|-info)
+      if [[ "$1" != '-info' ]]; then
+        TOOL_INFO="${1//*=}"
+      elif [[ $# -gt 1 && -d "$2" ]]; then
+        TOOL_INFO="$2"
+        shift
+      else
+        Die "Need argument for $1"
+      fi
+      shift;
+      ;;
     -i|--install=*|--apps=)
       if [[ "$1" != '-i' ]]; then
         APPS="${1//*=}"
@@ -569,6 +718,17 @@ function GetBuildOpts() {
     --home)
       APPS="${HOME}"
       SRC="${HOME}/src"
+      shift
+      ;;
+    -pref|--prefix=*)
+      if [[ "$1" != '-pref' ]]; then
+        CMAKE_INSTALL_PREFIX="${1//*=}"
+      elif [[ $# -gt 1 && -d "$2" ]]; then
+        CMAKE_INSTALL_PREFIX="$2"
+        shift
+      else
+        Die "Need argument for $1"
+      fi
       shift
       ;;
     -s|--src=*)
@@ -586,6 +746,17 @@ function GetBuildOpts() {
       CMAKE_CXX_STANDARD="${1//*=}"
       shift;
       ;;
+    --systemc=*|-sc)
+      if [[ "$1" != '-sc' ]]; then
+        SYSTEMC_HOME="${1//*=}"
+      elif [[ $# -gt 1 && -d "$2" ]]; then
+        SYSTEMC_HOME="$2"
+        shift
+      else
+        Die "Need argument for $1"
+      fi
+      shift;
+      ;;
     --suffix=*|-suf)
       if [[ "$1" != '-suf' ]]; then
         SUFFIX="${1//*=}"
@@ -597,8 +768,34 @@ function GetBuildOpts() {
       fi
       shift;
       ;;
+    --tool=*|-tool)
+      if [[ "$1" != '-tool' ]]; then
+        TOOL_NAME="${1//*=}"
+      elif [[ $# -gt 1 && -d "$2" ]]; then
+        TOOL_NAME="$2"
+        shift
+      else
+        Die "Need argument for $1"
+      fi
+      shift;
+      ;;
     --uninstall|-rm)
       CLEANUP=1;
+      shift;
+      ;;
+    --url=*|-url)
+      TOOL_URL="${1//*=}"
+      shift;
+      ;;
+    --version=*|-vers)
+      if [[ "$1" != '-vers' ]]; then
+        TOOL_VERS="${1//*=}"
+      elif [[ $# -gt 1 && -d "$2" ]]; then
+        TOOL_VERS="$2"
+        shift
+      else
+        Die "Need argument for $1"
+      fi
       shift;
       ;;
     *)
@@ -609,10 +806,33 @@ function GetBuildOpts() {
   done
 
   #-------------------------------------------------------------------------------
-  # Setup apps directory
+  # Defaults if not set
   if [[ -z "${APPS}" || ! -d "${APPS}" ]]; then
     APPS="${HOME}/.local/apps"
   fi
+  if [[ -z "${SYSTEMC_HOME}" ]]; then
+    SYSTEMC_HOME="${APPS}/systemc"
+  fi
+  if [[ -z "${CMAKE_INSTALL_PREFIX}" ]]; then
+    CMAKE_INSTALL_PREFIX="${APPS}"
+  fi
+  if [[ -z "${GENERATOR}" ]]; then
+    GENERATOR=cmake
+  fi
+  if [[ -z "${BUILD_DIR}" ]]; then
+    BUILD_DIR="build-${GENERATOR}-$(basename "${CC}")"
+  fi
+
+  #-------------------------------------------------------------------------------
+  # Test some assumptions
+  if [[ "${GENERATOR}" =~ cmake|autotools ]]; then
+    Comment all is ok
+  else
+    Error "GENERATOR must be one of 'cmake' or 'autotools'"
+  fi
+
+  #-------------------------------------------------------------------------------
+  # Setup apps directory
   mkdir -p "${APPS}" || Die "Failed to find/create ${APPS} directory"
 
   #-------------------------------------------------------------------------------
@@ -658,6 +878,66 @@ function SetupLogdir() {
   esac
 }
 
+function Create_and_Cd() {
+  Assert $# = 1
+  _do mkdir -p "${1}" || Die "Unable to create ${1}"
+  _do cd "${1}" || Die "Unable to enter ${1} directory"
+}
+
+function GetSource_and_Cd() {
+  Assert $# = 2
+  if [[ -n "${CLEAN}" && "${CLEAN}" == 1 ]]; then
+    _do rm -fr "${1}"
+  fi
+  if [[ -d "${1}/.git" ]]; then
+    _do git pull
+  else
+    if [[ -d "${1}/." ]]; then
+      _do mkdir -p "${1}-save"
+      _do rsync -a "${1}/" "${1}-save/"
+      _do rm -fr "${1}" 
+    fi
+    _do git clone "${2}" "${1}" || Die "Unable to clone into ${1}"
+  fi
+  cd "${1}" || Die "Unable to enter ${1}"
+  if [[ -s "${TOOL_VERS}" ]]; then
+    _do git  checkout "${TOOL_VERS}"
+  fi
+}
+
+function Generate() {
+  Assert $# = 0
+  case "${GENERATOR}" in
+    cmake)
+      _do rm -fr "${BUILD_DIR}"
+      _do mkdir -p "${BUILD_DIR}" || Die "Unable to create build directory"
+      _do cd "${BUILD_DIR}" || Die "Unable to enter ${BUILD_DIR} directory"
+      _do cmake\
+          -DCMAKE_INSTALL_PREFIX="${CMAKE_INSTALL_PREFIX}"\
+          -DCMAKE_CXX_STANDARD="${CMAKE_CXX_STANDARD}"\
+          ..
+      ;;
+    autotools)
+      reconfigure
+      _do mkdir -p "${BUILD_DIR}"
+      _do cd "${BUILD_DIR}" || Die "Unable to enter ${BUILD_DIR}"
+      _do env CXXFLAGS="-std=c++${CMAKE_CXX_STANDARD} -I/opt/local/include -I${SYSTEMC_HOME}/include"\
+          ../configure --prefix="${SYSTEMC_HOME}"
+      ;;
+    *)
+      Error "Unknown generator ${1}"
+      ;;
+  esac
+}
+
+function Cleanup() {
+  Assert $# = 1
+  if [[ -n "${CLEANUP}" && "${CLEANUP}" == 1 ]]; then
+    cd "${SRC}" || Die "Unable to enter source directory"
+    rm -fr "${1}"
+  fi
+}
+
 function Main() {
   Assert "${BASH_VERSINFO[0]}" -ge 5
   if [[ $# != 0 ]]; then
@@ -671,4 +951,4 @@ function Main() {
 
 Main "$@"
 
-# The end
+# vim:nospell
