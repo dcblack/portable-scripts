21fb2c6f68dc198d13e347dc3ef07817ce45d24d
David C Black <david.black@doulos.com>
2021-09-08 07:49:36 -0500

    Add zsh compatibility and fix NOAUTORUN handling

---
+++ .local/apps/sc-templates/cpp-main/lxNAMExr.cpp
@@ -0,0 +1,30 @@
+/** @brief {:BRIEF:}
+ *
+ * {:LONGER-DESCRIPTION:}
+ */
+
+#if __cplusplus < 201703L /* Verify minimum C++ */
+#error Requires at least C++17
+#endif
+
+#include "{:lc:NAME:}.hpp" /*< {:** DELETE IF NOT USED **:} */
+//{:PROJECT_INCLUDES_HERE:} {:** DELETE IF NOT USED **:}
+#include <string>
+#include <iostream>
+
+using namespace std::literals;
+
+int main( [[maybe_unused]]int argc, [[maybe_unused]]const char* argv[] )
+{
+  size_t status{ 0 };
+
+  //{:BODY_HERE:}
+
+  if ( status == 0 ) std::cout << "Success" << std::endl;
+  else               std::cout << "FAILURE" << std::endl;
+  return status?1:0;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// Copyright {:YEAR:} by {:COMPANY:}. All rights reserved.
+//END {:FILE:} {:Id:}
---
+++ .local/tests/ABOUT.md
@@ -0,0 +1,6 @@
+This directory contains testing scripts used to validate the operation of
+scripts elsewhere in this repository. All of this is automated use the
+`GNUmakefile`.
+
+All of the tests are ad-hoc in nature. Phony targets should be added to the
+`GNUmakefile` as test are added.
