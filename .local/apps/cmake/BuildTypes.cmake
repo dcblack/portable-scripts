e0cf499504b4a087e7e2d5a805fb28667ac2ff97
David C Black <david.black@doulos.com>
2021-12-22 04:43:47 -0600

    Move build type into conditional
    Avoid setting global variable if already defined.
    
    Signed-off-by: David C Black <david.black@doulos.com>

--- .local/apps/cmake/BuildTypes.cmake
+++ .local/apps/cmake/BuildTypes.cmake
@@ -1,11 +1,11 @@
 # Set a default build type if none was specified
-set(default_build_type "Release")
-if(EXISTS "${CMAKE_SOURCE_DIR}/.git")
-  message(STATUS "Building debug")
-  set(default_build_type "Debug")
-endif()
  
 if(NOT CMAKE_BUILD_TYPE AND NOT CMAKE_CONFIGURATION_TYPES)
+  set(default_build_type "Release")
+  if(EXISTS "${CMAKE_SOURCE_DIR}/.git")
+    message(STATUS "Building debug")
+    set(default_build_type "Debug")
+  endif()
   message(STATUS "Setting build type to '${default_build_type}' as none was specified.")
   set(CMAKE_BUILD_TYPE "${default_build_type}" CACHE
       STRING "Choose the type of build." FORCE)
