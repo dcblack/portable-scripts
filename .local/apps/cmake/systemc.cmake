c9735685bd714591608ec2392ea44949e549843e
David C Black <david.black@doulos.com>
2022-03-11 21:15:02 -0600

    Simplify
    Remove unnecesary crud.
    
    Signed-off-by: David C Black <david.black@doulos.com>

--- .local/apps/cmake/systemc.cmake
+++ .local/apps/cmake/systemc.cmake
@@ -1,25 +1,9 @@
 #!cmake .
 
-# Any important Modern C++ defaults
-# add_compile_definitions( )
-
 # SystemC specific needs
-set(CMAKE_PREFIX_PATH $ENV{SYSTEMC_HOME})
-find_package(SystemCLanguage CONFIG REQUIRED)
-set (CMAKE_CXX_STANDARD ${SystemC_CXX_STANDARD} CACHE STRING
-     "C++ standard to build all targets. Supported values are 98, 11, and 14.")
-set (CMAKE_CXX_STANDARD_REQUIRED ${SystemC_CXX_STANDARD_REQUIRED} CACHE BOOL
-     "The CMAKE_CXX_STANDARD selected C++ standard is a requirement.")
-link_libraries(SystemC::systemc)
+find_package( SystemCLanguage CONFIG REQUIRED PATHS "$ENV{SYSTEMC_HOME}" )
 
-# Things that affect all targets
-set(shared_env LD_LIBRARY_PATH=${CMAKE_PREFIX_PATH}/lib DYLD_LIBRARY_PATH=${CMAKE_PREFIX_PATH}/lib)
+set (CMAKE_CXX_STANDARD ${SystemC_CXX_STANDARD} CACHE STRING "C++ standard to build all targets. Supported values are 98, 11, 14, and 17.")
+set (CMAKE_CXX_STANDARD_REQUIRED ${SystemC_CXX_STANDARD_REQUIRED} CACHE BOOL "The CMAKE_CXX_STANDARD selected C++ standard is a requirement.")
 
-#find_package( Boost 1.65 REQUIRED )
-#find_package( yaml-cpp CONFIG REQUIRED )
-
-add_custom_target(run
-   time env ${shared_env} ./${TARGET_EXECUTABLE} ${ARGS} 2>&1 | tee ${TARGET_EXECUTABLE}.log
-   COMMENT "Running program ${TARGET_EXECUTABLE}"
-   USES_TERMINAL
-   )
+link_libraries( SystemC::systemc )
