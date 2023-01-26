4fcac6b960cc595844c3f68d735242004d392789
David C Black <david.black@doulos.com>
2023-01-26 07:31:31 -0600

    tweak. unfinished idea

--- .local/scratch/history.sh
+++ .local/scratch/history.sh
@@ -1,8 +1,9 @@
-sudo yum install dh-autoreconf curl-devel expat-devel gettext-devel   openssl-devel perl-devel zlib-devel
-sudo yum install asciidoc
-sudo yum install xmlto
-sudo yum install docbook2X
-sudo yum install gtk3-devel
-sudo yum search X11-devel
-sudo yum install libX11-devel
-./configure --prefix $HOME/.local --enable-gui=auto --with-x
+#!/usr/bin/bash
+
+sudo yum install -y dh-autoreconf curl-devel expat-devel gettext-devel   openssl-devel perl-devel zlib-devel
+sudo yum install -y asciidoc
+sudo yum install -y xmlto
+sudo yum install -y docbook2X
+sudo yum install -y gtk3-devel
+sudo yum install -y libX11-devel
+./configure --prefix "{$HOME}/.local" --enable-gui=auto --with-x
