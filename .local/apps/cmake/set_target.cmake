5abf07aa9a8d806664f6d93c7047387460329be1
David C Black <david.black@doulos.com>
2023-02-15 15:08:56 -0600

    Synchronize cmake with esscpp

--- .local/apps/cmake/set_target.cmake
+++ .local/apps/cmake/set_target.cmake
@@ -1,8 +1,18 @@
-cmake_minimum_required( VERSION "3.20" )
+cmake_minimum_required ( VERSION 3.17 )
 
 macro( set_target target )
-  set( TARGET "${target}" )
-  list( APPEND TARGETS "${target}" )
+  set( Target "${target}" )
+  list( LENGTH Targets count )
+  if( ${count} EQUAL 1)
+    list( GET Targets 0 first )
+    if( "${first}" STREQUAL "${PROJECT_NAME}" )
+      list( REMOVE_AT Targets 0 )
+    endif()
+  endif()
+  list( APPEND Targets "${target}" )
 endmacro()
+
+# Default
+set_target( ${PROJECT_NAME} )
 
 # vim:nospell
