2791ba700eeaf9bd6d721d3f66a987d3ab9d0ef4
David C Black <david.black@doulos.com>
2023-12-27 15:03:30 -0600

    Merge update

--- .local/scratch/history.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-
-sudo yum install -y dh-autoreconf curl-devel expat-devel gettext-devel   openssl-devel perl-devel zlib-devel
-sudo yum install -y asciidoc
-sudo yum install -y xmlto
-sudo yum install -y docbook2X
-sudo yum install -y gtk3-devel
-sudo yum install -y libX11-devel
-./configure --prefix "{$HOME}/.local" --enable-gui=auto --with-x
